f7ff3d2e30aa2da5feb7db79d22c32610825d198
Nikhitha-Sriram <120003140+Nikhitha-Sriram@users.noreply.github.com>
2022-12-07 14:34:36 +0530

    feat : product listing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+
+  const getProducts = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  };
   return (
     <div className="App">
-      
+      <main className="container">
+       {
+        products.map((data)=>
+        <Card maxW="xs" className="card">
+          <CardBody>
+            <Image
+            className="card-img"
+              src={data.image}
+              alt="product"
+              borderRadius="lg"
+              />
+            <Stack mt="6" spacing="3">
+              <Heading size="md">{data.title}</Heading>
+              <Text >
+                {data.description}
+              </Text>
+              <Text color="blue.600" fontSize="2xl">
+                Rs {data.price}
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button variant="solid" colorScheme="blue">
+                Buy now
+              </Button>
+              <Button variant="ghost" colorScheme="blue">
+                Add to cart
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+        )
+       }
+        
+      </main>
     </div>
   );
 }
